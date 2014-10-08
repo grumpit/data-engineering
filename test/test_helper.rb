5799865ef347b56ae0b0e58a3c252331b2a62337
Brian Hall <brian@codesherpas.com>
2014-10-08 11:10:37 -0400

    generate user model; require name to be present; generate custom devise views so name field may be added to form

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,6 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
+require 'simplecov'
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
@@ -7,4 +8,8 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+end
+
+class ActionController::TestCase
+  include Devise::TestHelpers
 end
