555e2efd9c50df035eec566f3f90cca4d00b391f
Brian Hall <brian@codesherpas.com>
2014-10-08 12:01:52 -0400

    add more tests for purchases

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,4 +1,7 @@
 class Purchase < ActiveRecord::Base
   belongs_to :purchaser
   belongs_to :item
+  
+  validates :purchaser_id, :item_id, presence: true
+  validates :quantity, numericality: { only_integer: true }
 end
--- test/models/purchase_test.rb
+++ test/models/purchase_test.rb
@@ -3,4 +3,8 @@ require 'test_helper'
 class PurchaseTest < ActiveSupport::TestCase
   should belong_to :purchaser
   should belong_to :item
+  
+  should validate_presence_of :purchaser_id
+  should validate_presence_of :item_id
+  should validate_numericality_of :quantity
 end
