39484dfe578ada0ee75876199eac6f19f6be5190
Brian Hall <brian@codesherpas.com>
2014-10-08 11:35:38 -0400

    add purchaser model and test

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141008144244) do
+ActiveRecord::Schema.define(version: 20141008153418) do
+
+  create_table "purchasers", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name",                                null: false
@@ -27,9 +33,11 @@ ActiveRecord::Schema.define(version: 20141008144244) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "identity_url"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["identity_url"], name: "index_users_on_identity_url", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
